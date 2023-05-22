michaelsproul/nimbus-eth2

Block / REST explorer

beacon_chain/spec/forks.nim
@@ -459,27 +459,38 @@ template getForkedBlockField*(
   of BeaconBlockFork.Altair:    unsafeAddr x.altairData.message.y
   of BeaconBlockFork.Bellatrix: unsafeAddr x.bellatrixData.message.y)[]
 
-template signature*(x: ForkedSignedBeaconBlock |
+template getForkedBodyField*(
+    x: ForkedSignedBeaconBlock |
+       ForkedMsgTrustedSignedBeaconBlock |
+       ForkedTrustedSignedBeaconBlock,
+    y: untyped): untyped =
+  # unsafeAddr avoids a copy of the field in some cases
+  (case x.kind
+  of BeaconBlockFork.Phase0:    unsafeAddr x.phase0Data.message.body.y
+  of BeaconBlockFork.Altair:    unsafeAddr x.altairData.message.body.y
+  of BeaconBlockFork.Bellatrix: unsafeAddr x.bellatrixData.message.body.y)[]
+
+func signature*(x: ForkedSignedBeaconBlock |
                        ForkedMsgTrustedSignedBeaconBlock): ValidatorSig =
   withBlck(x): blck.signature
 
-template signature*(x: ForkedTrustedSignedBeaconBlock): TrustedSig =
+func signature*(x: ForkedTrustedSignedBeaconBlock): TrustedSig =
   withBlck(x): blck.signature
 
-template root*(x: ForkedSignedBeaconBlock |
+func root*(x: ForkedSignedBeaconBlock |
                   ForkedMsgTrustedSignedBeaconBlock |
                   ForkedTrustedSignedBeaconBlock): Eth2Digest =
   withBlck(x): blck.root
 
-template slot*(x: ForkedSignedBeaconBlock |
+func slot*(x: ForkedSignedBeaconBlock |
                   ForkedMsgTrustedSignedBeaconBlock |
                   ForkedTrustedSignedBeaconBlock): Slot =
   withBlck(x): blck.message.slot
 
-template shortLog*(x: ForkedBeaconBlock): auto =
+func shortLog*(x: ForkedBeaconBlock): auto =
   withBlck(x): shortLog(blck)
 
-template shortLog*(x: ForkedSignedBeaconBlock |
+func shortLog*(x: ForkedSignedBeaconBlock |
                       ForkedMsgTrustedSignedBeaconBlock |
                       ForkedTrustedSignedBeaconBlock): auto =
   withBlck(x): shortLog(blck)

ngui/mainmodel.nim
@@ -1,0 +1,145 @@
+import
+  NimQml,
+  "."/[
+    blockmodel, footermodel, epochmodel, peerlist, slotlist, nodemodel,
+    poolmodel]
+
+import
+  std/[os, strutils],
+  chronos, metrics,
+
+  # Local modules
+  ../beacon_chain/spec/eth2_apis/rest_beacon_client,
+  ../beacon_chain/spec/datatypes/[phase0, altair],
+  ../beacon_chain/spec/[eth2_merkleization, helpers]
+
+QtObject:
+  type MainModel* = ref object of QObject
+    app: QApplication
+    blck: BlockModel
+    footer: FooterModel
+    client: RestClientRef
+    peerList: PeerList
+    epochModel: EpochModel
+    nodeModel: NodeModel
+    poolModel: PoolModel
+
+    genesis: RestGenesis
+    currentIndex: int
+
+  proc delete*(self: MainModel) =
+    self.QObject.delete
+    self.blck.delete
+
+  proc setup(self: MainModel) =
+    self.QObject.setup
+    self.blck.setup
+
+  proc newMainModel*(app: QApplication, url: string): MainModel =
+    let
+      client = RestClientRef.new(url).get()
+
+    var
+      headBlock = (waitFor client.getBlockV2(BlockIdent.init(BlockIdentType.Head), defaultRuntimeConfig)).get()
+      epoch = getForkedBlockField(headBlock[], slot).epoch
+      genesis = (waitFor client.getGenesis()).data.data
+      peerList = newPeerList(@[])
+
+    let res = MainModel(
+      app: app,
+      blck: newBlockModel(headBlock[], genesis.genesis_time),
+      client: client,
+      footer: newFooterModel(),
+      peerList: peerList,
+      epochModel: newEpochModel(client, epoch.int),
+      nodeModel: newNodeModel(client),
+      poolModel: newPoolModel(client),
+      genesis: genesis,
+    )
+    res.setup()
+    res
+
+  proc onExitTriggered(self: MainModel) {.slot.} =
+    self.app.quit
+
+  proc updateFooter(self: MainModel) {.slot.} =
+    let
+      checkpoints = (waitFor self.client.getStateFinalityCheckpoints(StateIdent.init(StateIdentType.Head))).data.data
+      head = (waitFor self.client.getBlockHeader(BlockIdent.init(BlockIdentType.Head))).data.data
+      syncing = (waitFor self.client.getSyncingStatus()).data.data
+
+    self.footer.finalized = $shortLog(checkpoints.finalized)
+    self.footer.head = $shortLog(head.header.message.slot)
+    self.footer.syncing = $syncing
+
+  proc updateSlots(self: MainModel) {.slot.} =
+    let
+      slots = self.client.loadSlots(self.epochModel.epoch.Epoch)
+    self.epochModel.setNewData(self.epochModel.epoch.int, slots)
+
+  proc updatePeers(self: MainModel) {.slot.} =
+    try:
+      self.peerList.setNewData(waitFor(self.client.getPeers(@[], @[])).data.data)
+    except CatchableError as exc:
+      echo exc.msg
+
+  proc getPeerList*(self: MainModel): QVariant {.slot.} =
+    newQVariant(self.peerList)
+  QtProperty[QVariant] peerList:
+    read = getPeerList
+
+  proc getFooter*(self: MainModel): QVariant {.slot.} =
+    newQVariant(self.footer)
+  QtProperty[QVariant] footer:
+    read = getFooter
+
+  proc getEpochModel*(self: MainModel): QVariant {.slot.} =
+    newQVariant(self.epochModel)
+  QtProperty[QVariant] epochModel:
+    read = getEpochModel
+
+  proc getBlck(self: MainModel): QVariant {.slot.} = newQVariant(self.blck)
+  proc blckChanged*(self: MainModel, blck: QVariant) {.signal.}
+  proc setBlck(self: MainModel, blck: ForkedSignedBeaconBlock) =
+    self.blck.blck = blck
+    self.blckChanged(newQVariant(self.blck))
+
+  QtProperty[QVariant] blck:
+    read = getBlck
+    write = setBlck
+    notify = blckChanged
+
+  proc getCurrentIndex(self: MainModel): int {.slot.} = self.currentIndex
+  proc currentIndexChanged*(self: MainModel, v: int) {.signal.}
+  proc setCurrentIndex(self: MainModel, v: int) =
+    self.currentIndex = v
+    self.currentIndexChanged(v)
+
+  QtProperty[int] currentIndex:
+    read = getCurrentIndex
+    write = setCurrentIndex
+    notify = currentIndexChanged
+
+  proc getNodeModel(self: MainModel): QVariant {.slot.} = newQVariant(self.nodeModel)
+  QtProperty[QVariant] nodeModel:
+    read = getNodeModel
+
+  proc getPoolModel(self: MainModel): QVariant {.slot.} = newQVariant(self.poolModel)
+  QtProperty[QVariant] poolModel:
+    read = getPoolModel
+
+  proc onLoadBlock(self: MainModel, root: string) {.slot.} =
+    try:
+      var blck = waitFor(self.client.getBlockV2(
+        BlockIdent.decodeString(root).tryGet(), defaultRuntimeConfig))
+      if blck.isSome():
+        self.setBlck(blck.get()[])
+
+    except CatchableError as exc:
+      echo exc.msg
+    discard
+
+  proc openUrl(self: MainModel, url: string) {.slot.} =
+    if url.startsWith("block://"):
+      self.onLoadBlock(url[8..^1])
+      self.setCurrentIndex(1)

vacp2p/nimbus-eth2-experimental

beacon_chain/spec/forks.nim
@@ -778,28 +778,41 @@ template getForkedBlockField*(
   of ConsensusFork.Capella:   unsafeAddr x.capellaData.message.y
   of ConsensusFork.Deneb:     unsafeAddr x.denebData.message.y)[]
 
-template signature*(x: ForkedSignedBeaconBlock |
+template getForkedBodyField*(
+    x: ForkedSignedBeaconBlock |
+       ForkedMsgTrustedSignedBeaconBlock |
+       ForkedTrustedSignedBeaconBlock,
+    y: untyped): untyped =
+  # unsafeAddr avoids a copy of the field in some cases
+  (case x.kind
+  of ConsensusFork.Phase0:    unsafeAddr x.phase0Data.message.body.y
+  of ConsensusFork.Altair:    unsafeAddr x.altairData.message.body.y
+  of ConsensusFork.Bellatrix: unsafeAddr x.bellatrixData.message.body.y
+  of ConsensusFork.Capella:   unsafeAddr x.capellaData.message.body.y
+  of ConsensusFork.Deneb:     unsafeAddr x.denebData.message.body.y)[]
+
+func signature*(x: ForkedSignedBeaconBlock |
                        ForkedMsgTrustedSignedBeaconBlock |
                        ForkedSignedBlindedBeaconBlock): ValidatorSig =
   withBlck(x): blck.signature
 
-template signature*(x: ForkedTrustedSignedBeaconBlock): TrustedSig =
+func signature*(x: ForkedTrustedSignedBeaconBlock): TrustedSig =
   withBlck(x): blck.signature
 
-template root*(x: ForkedSignedBeaconBlock |
+func root*(x: ForkedSignedBeaconBlock |
                   ForkedMsgTrustedSignedBeaconBlock |
                   ForkedTrustedSignedBeaconBlock): Eth2Digest =
   withBlck(x): blck.root
 
-template slot*(x: ForkedSignedBeaconBlock |
+func slot*(x: ForkedSignedBeaconBlock |
                   ForkedMsgTrustedSignedBeaconBlock |
                   ForkedTrustedSignedBeaconBlock): Slot =
   withBlck(x): blck.message.slot
 
-template shortLog*(x: ForkedBeaconBlock | ForkedBlindedBeaconBlock): auto =
+func shortLog*(x: ForkedBeaconBlock | ForkedBlindedBeaconBlock): auto =
   withBlck(x): shortLog(blck)
 
-template shortLog*(x: ForkedSignedBeaconBlock |
+func shortLog*(x: ForkedSignedBeaconBlock |
                       ForkedMsgTrustedSignedBeaconBlock |
                       ForkedTrustedSignedBeaconBlock |
                       ForkedSignedBlindedBeaconBlock): auto =

ngui/mainmodel.nim
@@ -1,0 +1,147 @@
+import
+  NimQml,
+  "."/[
+    blockmodel, footermodel, epochmodel, peerlist, slotlist, nodemodel,
+    poolmodel]
+
+import
+  std/[os, strutils],
+  chronos, metrics,
+
+  # Local modules
+  ../beacon_chain/spec/eth2_apis/rest_beacon_client,
+  ../beacon_chain/spec/datatypes/[phase0, altair],
+  ../beacon_chain/spec/[eth2_merkleization, helpers]
+
+QtObject:
+  type MainModel* = ref object of QObject
+    app: QApplication
+    cfg: RuntimeConfig
+    blck: BlockModel
+    footer: FooterModel
+    client: RestClientRef
+    peerList: PeerList
+    epochModel: EpochModel
+    nodeModel: NodeModel
+    poolModel: PoolModel
+
+    genesis: RestGenesis
+    currentIndex: int
+
+  proc delete*(self: MainModel) =
+    self.QObject.delete
+    self.blck.delete
+
+  proc setup(self: MainModel) =
+    self.QObject.setup
+    self.blck.setup
+
+  proc newMainModel*(app: QApplication, url: string, cfg: RuntimeConfig): MainModel =
+    let
+      client = RestClientRef.new(url).get()
+
+    var
+      headBlock = (waitFor client.getBlockV2(BlockIdent.init(BlockIdentType.Head), cfg)).get()
+      epoch = getForkedBlockField(headBlock[], slot).epoch
+      genesis = (waitFor client.getGenesis()).data.data
+      peerList = newPeerList(@[])
+
+    let res = MainModel(
+      app: app,
+      cfg: cfg,
+      blck: newBlockModel(headBlock[], genesis.genesis_time),
+      client: client,
+      footer: newFooterModel(),
+      peerList: peerList,
+      epochModel: newEpochModel(client, epoch.int),
+      nodeModel: newNodeModel(client),
+      poolModel: newPoolModel(client),
+      genesis: genesis,
+    )
+    res.setup()
+    res
+
+  proc onExitTriggered(self: MainModel) {.slot.} =
+    self.app.quit
+
+  proc updateFooter(self: MainModel) {.slot.} =
+    let
+      checkpoints = (waitFor self.client.getStateFinalityCheckpoints(StateIdent.init(StateIdentType.Head))).data.data
+      head = (waitFor self.client.getBlockHeader(BlockIdent.init(BlockIdentType.Head))).data.data
+      syncing = (waitFor self.client.getSyncingStatus()).data.data
+
+    self.footer.finalized = $shortLog(checkpoints.finalized)
+    self.footer.head = $shortLog(head.header.message.slot)
+    self.footer.syncing = $syncing
+
+  proc updateSlots(self: MainModel) {.slot.} =
+    let
+      slots = self.client.loadSlots(self.epochModel.epoch.Epoch)
+    self.epochModel.setNewData(self.epochModel.epoch.int, slots)
+
+  proc updatePeers(self: MainModel) {.slot.} =
+    try:
+      self.peerList.setNewData(waitFor(self.client.getPeers(@[], @[])).data.data)
+    except CatchableError as exc:
+      echo exc.msg
+
+  proc getPeerList*(self: MainModel): QVariant {.slot.} =
+    newQVariant(self.peerList)
+  QtProperty[QVariant] peerList:
+    read = getPeerList
+
+  proc getFooter*(self: MainModel): QVariant {.slot.} =
+    newQVariant(self.footer)
+  QtProperty[QVariant] footer:
+    read = getFooter
+
+  proc getEpochModel*(self: MainModel): QVariant {.slot.} =
+    newQVariant(self.epochModel)
+  QtProperty[QVariant] epochModel:
+    read = getEpochModel
+
+  proc getBlck(self: MainModel): QVariant {.slot.} = newQVariant(self.blck)
+  proc blckChanged*(self: MainModel, blck: QVariant) {.signal.}
+  proc setBlck(self: MainModel, blck: ForkedSignedBeaconBlock) =
+    self.blck.blck = blck
+    self.blckChanged(newQVariant(self.blck))
+
+  QtProperty[QVariant] blck:
+    read = getBlck
+    write = setBlck
+    notify = blckChanged
+
+  proc getCurrentIndex(self: MainModel): int {.slot.} = self.currentIndex
+  proc currentIndexChanged*(self: MainModel, v: int) {.signal.}
+  proc setCurrentIndex(self: MainModel, v: int) =
+    self.currentIndex = v
+    self.currentIndexChanged(v)
+
+  QtProperty[int] currentIndex:
+    read = getCurrentIndex
+    write = setCurrentIndex
+    notify = currentIndexChanged
+
+  proc getNodeModel(self: MainModel): QVariant {.slot.} = newQVariant(self.nodeModel)
+  QtProperty[QVariant] nodeModel:
+    read = getNodeModel
+
+  proc getPoolModel(self: MainModel): QVariant {.slot.} = newQVariant(self.poolModel)
+  QtProperty[QVariant] poolModel:
+    read = getPoolModel
+
+  proc onLoadBlock(self: MainModel, root: string) {.slot.} =
+    try:
+      var blck = waitFor(self.client.getBlockV2(
+        BlockIdent.decodeString(root).tryGet(), self.cfg))
+      if blck.isSome():
+        self.setBlck(blck.get()[])
+
+    except CatchableError as exc:
+      echo exc.msg
+    discard
+
+  proc openUrl(self: MainModel, url: string) {.slot.} =
+    if url.startsWith("block://"):
+      self.onLoadBlock(url[8..^1])
+      self.setCurrentIndex(1)